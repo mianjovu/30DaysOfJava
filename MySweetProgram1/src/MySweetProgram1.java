40456f2787cd7ab8ddacad60f61e3bdd77fe9d95
Mia-Rose <mia-rose@Mia-Roses-iMac.local>
2020-10-24 22:40:43 +0100

    iterators

--- MySweetProgram1/src/MySweetProgram1.java
+++ MySweetProgram1/src/MySweetProgram1.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,17 +10,19 @@ import java.util.Scanner;
 public class MySweetProgram1 {
 	
 	public static void main(String[] args)  {
-		
-		
 		LinkedList<String> names = new LinkedList<String>();
 		names.push("Mia");
 		names.push("Sue");
 		names.push("Sally");
 		
-		System.out.println(names.pop());
-		System.out.println(names.removeFirst());
-		System.out.println(names.remove());
+		Iterator<String> it = names.iterator();
+	
+		names.add(2, "Susan");
 		
+		
+		for(String s: names) {
+			System.out.println(s);
+		}
 	}
 	
 }
